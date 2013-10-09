7dd433460842a7c8b5bfbe7e496c4920c9f7bed8
tran do <tranvandofet@gmail.com>
2013-10-09 13:40:04 +0700

    code read file .dat

---
+++ File_path/src/Demo.java
@@ -0,0 +1,45 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileWriter;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+
+
+public class Demo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		ArrayList<String> arr= new ArrayList<String>();
+		String ma ="tran     van     do";
+		
+        String file1 = "C:\\scanned72.dat";
+        //reading   
+        try{
+        	
+        	File f = new File(file1);
+            InputStream ips=new FileInputStream(file1); 
+            InputStreamReader ipsr=new InputStreamReader(ips);
+            BufferedReader br=new BufferedReader(ipsr);
+            String line;
+            while ((line=br.readLine())!=null){
+              arr.add(line+"\n");
+            }
+            br.close(); 
+            String[] temp=ma.split("     ");
+            System.out.print(arr);
+        	
+        }       
+        catch (Exception e){
+            System.out.println(e.toString());
+        }
+
+    }
+}
